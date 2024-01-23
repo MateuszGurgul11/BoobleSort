6e3c538fa99835c8388e3d9c6c5b5ce573d342a0
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-01-23 18:33:37 +0100

    Dodanie mozliwosci modyfikacji listy i wpisania wlasnych wartosci

--- boobleSort.py
+++ boobleSort.py
@@ -1,14 +1,17 @@
-def sort():
-    number_list = [5, 3, 6, 1, 6, 10, 22, 43, 55, 12]
+n = int(input("Wpisz wielkosc listy: "))
+number_list = []
 
-    list_size = len(number_list)
+for add_list in range(n):
+    number = int(input("Wpisz liczbe: "))
+    number_list.append(number)
 
-    for i in range(list_size):
-        for j in range(0, list_size - i -1):
-            if(number_list[j] > number_list[j + 1]):
-                number_list[j], number_list[j + 1] = number_list[j + 1], number_list[j]
-                print(number_list)
+list_size = len(number_list)
+
+for i in range(list_size):
+    for j in range(0, list_size - i -1):
+        if(number_list[j] > number_list[j + 1]):
+            number_list[j], number_list[j + 1] = number_list[j + 1], number_list[j]
+            print(number_list)
     
-    return number_list
 
-print(sort())
+
